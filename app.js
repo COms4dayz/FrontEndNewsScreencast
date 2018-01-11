c08fd8b46197d0bda5b621130a769ad5057cdeb6
COms4dayz <sparkman207@verizon.net>
2018-01-11 01:25:50 -0500

    modal and modal overlay

--- app.js
+++ app.js
@@ -6,9 +6,29 @@ $(document).ready(function(){
 		event.preventDefault();
 		$(this).text('Liked!').closest('.news-item').addClass('is-liked');
 	});
+	//-------------------------------------
+	// Link
+	//-------------------------------------
+
 
 	$('.js-add-link').on('click',function(event){
 		event.preventDefault();
 		$('.js-form').toggleClass('is-visible');
 	});
+
+	//-------------------------------------
+	// Modal
+	//-------------------------------------
+
+	$('.js-show-modal').on('click',function(event){
+		event.preventDefault();
+		$('.js-modal').addClass('is-visible');
+		$('.js-modal-overlay').addClass('is-visible');
+	});
+
+	$('.js-modal-overlay').on('click',function(event){
+		$('.js-modal').removeClass('is-visible');
+		$('.js-modal-overlay').removeClass('is-visible');
+	});
+
 });
