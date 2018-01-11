33e8a270842ba64b57b4ab95ed1c94cd51677af9
COms4dayz <sparkman207@verizon.net>
2018-01-10 20:13:48 -0500

    form animation and jQuery

--- app.js
+++ app.js
@@ -5,8 +5,10 @@ $(document).ready(function(){
 	$('.js-like').on('click',function(event){
 		event.preventDefault();
 		$(this).text('Liked!').closest('.news-item').addClass('is-liked');
-		
+	});
 
-
+	$('.js-add-link').on('click',function(event){
+		event.preventDefault();
+		$('.js-form').toggleClass('is-visible');
 	});
 });
